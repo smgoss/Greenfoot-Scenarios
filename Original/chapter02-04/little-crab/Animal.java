ba3351b72cea3c2df7d7ae8f3516d46930c544a3
smgoss <goss.stephenm@gmail.com>
2016-02-24 11:14:44 -0500

    Added animal class
    Added he animal class and overloaded the move method.

---
+++ Original/chapter02-04/little-crab/Animal.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Animal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Animal extends Actor
+{
+    /**
+     * Act - do whatever the Animal wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    /**
+    * move - overloaded to support function call with no variables
+    * passed in. Moves an animal one space.
+    */
+    public void move()
+    {
+        super.move(1);
+    }
+}
--- Original/chapter02-04/little-crab/Crab.java
+++ Original/chapter02-04/little-crab/Crab.java
@@ -3,11 +3,12 @@ import greenfoot.*;
 /**
  * This class defines a crab. Crabs live on the beach.
  */
-public class Crab extends Actor
+public class Crab extends Animal
 {
     public void act()
     {
         // Add your action code here
+        move(50);
     }
 }
 
